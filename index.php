35803403aee9312c57d6314f46a647379a5e88fc
FELIPE AUGUSTO STEC <felipe.8988@aluno.pr.senac.br>
2022-08-04 22:26:47 -0300

    add bootstrap

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello hell";
+
+function dd($input) {
+    var_dump($input);
+    die();
+}
+
+
+$mainPosition = __DIR__;
+
+require("{$mainPosition}\\bootstrap\Env.php");
+
+$env = new Env();
+dd($env);
