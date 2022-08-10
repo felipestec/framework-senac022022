abc8693b4da563b9a37c0c30b6996a326abaed51
FELIPE AUGUSTO STEC <felipe.8988@aluno.pr.senac.br>
2022-08-09 22:23:23 -0300

    subindo

--- bootstrap/Env.php
+++ bootstrap/Env.php
@@ -2,8 +2,24 @@
 
 class Env {
 
-    public function __construct(){
+        
+    public static function execute(){
 
+        $contentOfEnvFile = file_get_contents(__DIR__ . "\..\.env");
+        $arrayEnv = explode("\n",$contentOfEnvFile);
 
+        foreach ($arrayEnv as $value) {
+            $keyAndValue = explode("=", $value);
+
+            if (!isset($keyAndValue[1])) {      //isset = se existe (!isset negação - verifica se é falso)
+                continue;
+            }
+
+            $nameOfVariable = $keyAndValue[0];
+            $valueOfVariable = $keyAndValue[1];
+
+            $_ENV[$nameOfVariable] = $valueOfVariable; // $_ENV (array "mágico" local das variaveis globais) pode ser acessado de qualquer local
+        }
+               
     }
 }
