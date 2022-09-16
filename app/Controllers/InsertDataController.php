e99216cff33af028d4cd23724cef2eadceacf9de
FELIPE AUGUSTO STEC <felipe.8988@aluno.pr.senac.br>
2022-09-15 22:27:25 -0300

    aula 15/099

--- app/Controllers/InsertDataController.php
+++ app/Controllers/InsertDataController.php
@@ -3,12 +3,25 @@
 namespace App\Controllers;
 
 use App\FrameworkTools\Abstracts\Controllers\AbstractControllers;
-use App\FrameworkTools\DatabaseConnection;
+use App\FrameworkTools\Database\DatabaseConnection;
 
 class InsertDataController extends AbstractControllers{
 
     public function exec (){
-       
+        $pdo = DatabaseConnection::start()->getPDO();
+        //dd($pdo); 
+        $params = $this->processServerElements->getInputJSONData();
+       // dd($params);
+       $query = "INSERT INTO user (name,last_name,age) VALUES (:name,:last_name,:age)";
+
+       $statement = $pdo->prepare($query);
+
+       $statement->execute([
+            ':name' => $params["name"],
+            ':last_name' => $params["lastName"],
+            ':age' => $params["age"]
+       ]);
+
         view([
             "sucess" => true         
             
