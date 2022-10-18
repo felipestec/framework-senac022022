1c868f5bdc288490b2770f502970780b6ca11172
FELIPE AUGUSTO STEC <felipe.8988@aluno.pr.senac.br>
2022-10-18 20:58:25 -0300

    trabalho html

--- sql/trabalhoPost.sql
+++ sql/trabalhoPost.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS trabalhoPost;
-CREATE DATABASE trabalhoPost;
-USE trabalhoPost;
+DROP DATABASE IF EXISTS frameworksenac;
+CREATE DATABASE frameworksenac;
+USE frameworksenac;
 
 
 CREATE TABLE IF NOT EXISTS car(
